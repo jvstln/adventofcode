24b0e8660a0003d74c5b5201c61aa57c2b207d98
jvstln <173433315+jvstln@users.noreply.github.com>
2024-11-26 18:10:54 +0100

    Complete 2015 day-5 part 2
    Modified the code such that part 2 replaces part 1
    To see part 1 solution, checkout to the previous commit

--- 2015/day-5/index.js
+++ 2015/day-5/index.js
@@ -5,13 +5,10 @@ const input = await readFile(`${import.meta.dirname}/input.txt`, "utf8");
 let niceCount = 0;
 
 for (const string of input.split("\n")) {
-  const containsVowels = /([aeiou].*){3,}/g.test(string);
-  const containDupes = /(.)\1/.test(string);
-  const containExceptions = ["ab", "cd", "pq", "xy"].some((exception) =>
-    string.includes(exception)
-  );
+  const containTwoDupes = /(.{2}).*\1/.test(string);
+  const containMiddleDupes = /(.).\1/.test(string);
 
-  if (containsVowels && containDupes && !containExceptions) {
+  if (containTwoDupes && containMiddleDupes) {
     niceCount++;
   }
 }
