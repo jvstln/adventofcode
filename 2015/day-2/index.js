200828ae3607adee12029d9477d072a8533e4062
jvstln <173433315+jvstln@users.noreply.github.com>
2024-11-26 10:42:45 +0100

    Complete 2015 day-2 part 2

--- 2015/day-2/index.js
+++ 2015/day-2/index.js
@@ -3,13 +3,20 @@ import { readFile } from "fs/promises";
 const input = await readFile(`${import.meta.dirname}/input.txt`, "utf8");
 
 let total = 0;
+let totalRibbon = 0;
 
 for (const line of input.split("\n")) {
   const [l, w, h] = line.split("x").map((n) => Number(n));
 
   let area = 2 * l * w + 2 * w * h + 2 * h * l;
   let extra = Math.min(l * w, l * h, w * h);
+
+  let wrapRibbon = Math.min(2 * (l + w), 2 * (l + h), 2 * (w + h));
+  let bowRibbon = l * w * h;
+
   total += area + extra;
+  totalRibbon += wrapRibbon + bowRibbon;
 }
 
 console.log(total);
+console.log(totalRibbon);
