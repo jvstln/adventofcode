2fb9664e14d3c7e4998fbc6ed71ae66186e70b5b
jvstln <173433315+jvstln@users.noreply.github.com>
2024-11-26 11:02:17 +0100

    Complete 2015 day-2 part 2
    Modified the code such that part 2 replaces part 1
    To see part 1 solution, checkout to the previous commit

--- 2015/day-3/index.js
+++ 2015/day-3/index.js
@@ -3,9 +3,13 @@ import { readFile } from "fs/promises";
 const input = await readFile(`${import.meta.dirname}/input.txt`, "utf8");
 
 const visitedHouses = new Set(["0,0"]);
-let lastHouse = [0, 0];
+let SantaLastHouse = [0, 0];
+let RoboSantaLastHouse = [0, 0];
 
-for (const char of input) {
+for (let i = 0; i < input.length; i++) {
+  const lastHouse = i % 2 ? RoboSantaLastHouse : SantaLastHouse;
+  const char = input[i];
+
   switch (char) {
     case "^":
       lastHouse[1]++;
