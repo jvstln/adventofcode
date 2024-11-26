2c864fd9fec0884153db30e2007f7f90a579abed
jvstln <173433315+jvstln@users.noreply.github.com>
2024-11-26 10:17:42 +0100

    Complete 2015 day-1 part 2

--- 2015/day-1/index.js
+++ 2015/day-1/index.js
@@ -3,8 +3,18 @@ import { readFile } from "fs/promises";
 const input = await readFile(`${import.meta.dirname}/input.txt`, "utf8");
 
 let floor = 0;
+let hasReachedBasement = false;
+let firstBasementPosition = 0;
 for (const char of input) {
   floor += char == "(" ? 1 : -1;
+  if (hasReachedBasement) {
+    continue;
+  } else {
+    firstBasementPosition++;
+  }
+
+  if (floor == -1) hasReachedBasement = true;
 }
 
 console.log(floor);
+console.log(firstBasementPosition);
