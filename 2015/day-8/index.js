19b723543c1ba47e5242a3bd76e7cb3ca71d8958
jvstln <173433315+jvstln@users.noreply.github.com>
2024-11-30 16:13:02 +0100

    Complete 2015 day-8 part 2

--- 2015/day-8/index.js
+++ 2015/day-8/index.js
@@ -3,14 +3,19 @@ const input = await readFile(`${import.meta.dirname}/input.txt`, "utf8");
 
 let stringCodeSize = 0;
 let inMemorySize = 0;
+let reEncodedStringCodeCount = 0;
 
 for (const string of input.split("\n")) {
-  let transformedString = string
+  let decodedString = string
     .replace(/^"|"$/g, "")
     .replace(/\\("|\\|x[\da-f]{2})/g, (_, char) => char[0]);
 
+  let reEncodedString = `"${string.replace(/(\\|")/g, "\\$1")}"`;
+
   stringCodeSize += string.length;
-  inMemorySize += transformedString.length;
+  inMemorySize += decodedString.length;
+  reEncodedStringCodeCount += reEncodedString.length;
 }
 
-console.log(stringCodeSize - inMemorySize);
+console.log(stringCodeSize - inMemorySize); // part 1
+console.log(reEncodedStringCodeCount - stringCodeSize); // part 2
