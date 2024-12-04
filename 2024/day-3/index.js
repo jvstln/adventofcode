aae1bd78c4ee00404309da4373e665969466fbac
jvstln <173433315+jvstln@users.noreply.github.com>
2024-12-04 16:34:03 +0100

    Complete 2024 day-3 part 2
    Modified the code such that part 2 replaces part 1
    To see part 1 solution, checkout to the previous commit

--- 2024/day-3/index.js
+++ 2024/day-3/index.js
@@ -2,12 +2,16 @@ import { readFile } from "fs/promises";
 const input = await readFile(`${import.meta.dirname}/input.txt`, "utf8");
 
 let validSum = 0;
+let pauseOperation = false;
 
-input.match(/mul\(\d+,\d+\)/g).forEach((mulOperation) => {
-  let num1 = mulOperation.slice(4, mulOperation.indexOf(","));
-  let num2 = mulOperation.slice(mulOperation.indexOf(",") + 1, -1);
+input.match(/mul\(\d+,\d+\)|do\(\)|don't\(\)/g).forEach((operation) => {
+  if (operation == "don't()") return (pauseOperation = true);
+  if (operation == "do()") return (pauseOperation = false);
 
-  validSum += num1 * num2;
+  let num1 = operation.slice(4, operation.indexOf(","));
+  let num2 = operation.slice(operation.indexOf(",") + 1, -1);
+
+  if (!pauseOperation) validSum += num1 * num2;
 });
 
 console.log(validSum);
