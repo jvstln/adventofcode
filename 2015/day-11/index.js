9c57dfb36432b7a5faf8a1940c242df038d104fd
jvstln <173433315+jvstln@users.noreply.github.com>
2024-12-05 04:54:26 +0100

    Complete 2015 day-11 part 1

---
+++ 2015/day-11/index.js
@@ -0,0 +1,30 @@
+let input = "cqjxjnds";
+
+function nextSequence(input) {
+  const alphabet = "abcdefghijklmnoprstuvwxyz";
+  if (input.at(-1) == "z") return nextSequence(input.slice(0, -1)) + "a";
+  return input.slice(0, -1) + alphabet[alphabet.indexOf(input.at(-1)) + 1];
+}
+
+function validatePassword(password) {
+  let hasThreeLetterSequence = [...password].some(
+    (c, i, arr) =>
+      arr[i + 1] == nextSequence(c) && arr[i + 2] == nextSequence(arr[i + 1])
+  );
+
+  if (
+    /[iol]/.test(password) || // Contains at least one i, o, or l
+    (password.match(/(.)\1/g) ?? []).length < 2 || // Contains at least two adjacent matching characters
+    !hasThreeLetterSequence // Does NOT contain at least three letters in a row
+  ) {
+    return false;
+  }
+
+  return true;
+}
+
+while (!validatePassword(input)) {
+  input = nextSequence(input);
+}
+
+console.log(input);
